e200caae4dcb6805a30e48ece83d709e88f7e15f
Ainoa Murillo López <ainoa.murillo@hotmail.com>
2019-02-25 23:58:13 +0100

    Representar una vista con una plantilla de página

--- BasicProject/HelloDjangoApp/views.py
+++ BasicProject/HelloDjangoApp/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+    #html_content = "<html><head><title>Hello, Django</title></head><body>"
+    #html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A,%d %B, %Y at %X")
+    #html_content += "</body></html>"
+    #    return HttpResponse(html_content)
+
+    return render(
+        request,
+        "HelloDjangoApp/index.html", # Relative path from the 'templates' folder to the template file "index.html",
+        {
+            'title': "Hello Django",
+            'message': "Hello Django!",
+            'content': " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+)
