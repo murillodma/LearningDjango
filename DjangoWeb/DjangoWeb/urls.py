3462a43747d940b71bd796ac9dce4b4fd19c82e1
Ainoa Murillo López <ainoa.murillo@hotmail.com>
2019-03-25 14:20:16 +0100

    Añadido acceso a la vista admin

--- DjangoWeb/DjangoWeb/urls.py
+++ DjangoWeb/DjangoWeb/urls.py
@@ -6,13 +6,14 @@ from datetime import datetime
 from django.conf.urls import url
 import django.contrib.auth.views
 
+
 import app.forms
 import app.views
 
 # Uncomment the next lines to enable the admin:
-# from django.conf.urls import include
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls import include
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     # Examples:
@@ -39,8 +40,8 @@ urlpatterns = [
         name='logout'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 ]
